5b0ae2992fb1f57b3183fdd5dc1c78786c854134
eksplosion <kauandecarli@gmail.com>
2021-08-31 13:08:46 -0300

    improve 'cpu_temperature.sh'

--- cpu_temperature.sh
+++ cpu_temperature.sh
@@ -1,10 +1,9 @@
+#!/bin/bash
 let "temps = $(wc -l <<< $(ls /sys/devices/platform/coretemp.0/hwmon/hwmon?/temp*_input))"
 let "total = 0"
-let "i = 1"
-while [ $i -lt $temps ]; do
-    let "temp = $(cat /sys/devices/platform/coretemp.0/hwmon/hwmon?/temp$i\_input)"
+for files in $(ls /sys/devices/platform/coretemp.0/hwmon/hwmon?/temp*_input); do
+    let "temp = $(cat $files)"
     let "total = $total + $temp"
-    let "i = i + 1"
 done
-let "temp = total / ($i-1) / 1000"
+let "temp = $total / ($temps) / 1000"
 printf "$temp°C"
